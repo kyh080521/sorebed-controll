54b0e3e09fa94d43704a421250a70b97bd01bcd0
kyh080521 <kyh0805217@gmail.com>
2024-06-09 15:48:58 +0900

    2024-06-09

--- src/components/wardStateButton.js
+++ src/components/wardStateButton.js
@@ -6,7 +6,7 @@ import styles from '../styles/wardStateButton.module.css'
 const WardStateButton = ({state }) => {
     // state 값에 따라 색상 결정
     const getColor = () => {
-        if (state === 0) return 'red'; // fall이 1 이상이면 빨간색
+        if (state === 1) return 'red'; // fall이 1 이상이면 빨간색
         else return 'white'
     };
 
